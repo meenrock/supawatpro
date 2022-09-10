5472118f89a7258fa89caaddc26ca0eef21761f7
sstacker_m <jooxmeen2018@gmail.com>
2022-09-10 17:52:34 +0700

    Create files.js

---
+++ script/files.js
@@ -0,0 +1,12 @@
+const open = document.getElementById('btnexpress');
+const modal_container = document.getElementById('modal-container');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
+
